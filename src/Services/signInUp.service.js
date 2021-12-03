ebf6e1e426183a292c5a7193a02c4a5c974d271d
HarshilMendpara <mendparaharshil@gmail.com>
2021-12-03 18:53:39 +0530

    Initialized firebase app for otp auth
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

---
+++ src/Services/signInUp.service.js
@@ -0,0 +1,45 @@
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+
+export const firebaseAuthSendOTP = async (mobile, reCAPTCHA) => {
+    try {
+        const auth = getAuth();
+
+        if (reCAPTCHA) {
+            window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+                'size': 'invisible'
+            }, auth);
+        }
+
+        const phoneNumber = '+91' + mobile;
+        const appVerifier = window.recaptchaVerifier;
+
+        const confirmationresult = await signInWithPhoneNumber(auth, phoneNumber, appVerifier);
+        return confirmationresult;
+    } catch (error) {
+        if (error.code === 'auth/invalid-phone-number') {
+            alert('Mobile number Invalid, Refresh and try again.');
+        } else if (error.code === 'auth/too-many-requests') {
+            alert('Too many requests, Try again after some time.');
+        } else if (error.code === 'auth/quota-exceeded') {
+            alert('Quota exceeded, Try again after some time.');
+        } else {
+            alert('Error ocuured!');
+            console.log(error);
+        }
+    }
+}
+
+export const firebaseAuthConfirmOTP = async (password, confirmationResult) => {
+    try {
+        const result = await confirmationResult.confirm(password);
+        return result.user;
+    } catch (error) {
+        if (error.code === 'auth/invalid-verification-code') {
+            alert('OTP Invalid!');
+        } else {
+            alert('error ocuured!');
+            console.log(error);
+        }
+    }
+}
+
