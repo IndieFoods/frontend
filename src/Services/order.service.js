de55e13ebdf73306987564aef98411c03a023aa9
HarshilMendpara <mendparaharshil@gmail.com>
2021-12-04 21:35:51 +0530

    Added payment api functions
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

---
+++ src/Services/order.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+import { GET_USER_ORDERS, INITIALIZE_ORDER_URL } from "../Utils/constants";
+
+export const initializeOrder = async (chefId, type, address, numberOfPeople, numberOfWeeks, weeklySubscriptionAmount, accessToken) => {
+    try {
+        const { data } = await axios.post(
+            INITIALIZE_ORDER_URL,
+            {
+                chefId, type, isVeg: true, address, numberOfPeople: parseInt(numberOfPeople), numberOfWeeks: parseInt(numberOfWeeks), weeklySubscriptionAmount
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getOrdersOfAUser = async (accessToken) => {
+    try {
+        const { data } = await axios.get(
+            GET_USER_ORDERS,
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
