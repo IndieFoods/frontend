095a0c2ffdcbd33f6470b5cc49ca309cc7e87fb0
HarshilMendpara <mendparaharshil@gmail.com>
2021-12-04 21:40:15 +0530

    Stored chefDetails in Redux
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
-import { getAuth, onAuthStateChanged } from "@firebase/auth";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-
+import { Switch, Route, Redirect } from "react-router-dom";
 import { useHistory } from "react-router";
-import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
 import LandingPage from "./Containers/LandingPage";
-import { getUser } from "./Services/user.service";
 import UserHome from "./Containers/UserHome";
 import FoodSubDetails from "./Containers/FoodSubDetails";
+import UserProfile from "./Containers/UserProfile/UserProfile";
 import ChefHome from "./Containers/ChefHome";
-import ChefProfile from "./Containers/ChefProfile";
-import UserProfile from "./Containers/UserProfile";
 
-const fetchUserData = async (accessToken, dispatch) => {
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import ChefProfile from "./Containers/ChefProfile";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./Services/user.service";
+import { getChefs } from "./Services/chef.service";
+import PrivateRoute from "./Utils/helper/PrivateRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const fetchUserData = async (accessToken, uid, dispatch) => {
   dispatch({
     type: "UPDATE_ACCESS_TOKEN",
-    details: accessToken,
+    details: { accessToken, uid },
   });
 
   const userData = await getUser(accessToken);
+  const chefDetails = await getChefs();
 
   dispatch({
     type: "UPDATE_USER_DATA",
     details: userData,
   });
+  dispatch({
+    type: "UPDATE_CHEF_DATA",
+    details: chefDetails,
+  });
 };
 
 const App = () => {
-  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.userReducer.userData);
+
   const history = useHistory();
+  const location = history.location;
+  const dispatch = useDispatch();
   const auth = getAuth();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        fetchUserData(user.accessToken, dispatch);
+        // console.log(user);
+        let currentPage = history.location.pathname.split("/")[1];
+        if (
+          (currentPage !== "home" &&
+            currentPage !== "profile" &&
+            currentPage !== "chef") ||
+          (userData.isChef && currentPage === "chef")
+        ) {
+          history.push("/home");
+        }
+        fetchUserData(user.accessToken, user.uid, dispatch);
       } else {
         console.log("user is signed out");
         history.push("/");
@@ -43,15 +68,30 @@ const App = () => {
   }, []);
 
   return (
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route path={["/signin", "/signup"]} component={LandingPage} />
-      <Route exact path="/userhome" component={UserHome} />
-      <Route exact path="/chefhome" component={ChefHome} />
-      <Route exact path="/foodsubdetails" component={FoodSubDetails} />
-      <Route exact path="/chefProfile" component={ChefProfile} />
-      <Route exact path="/userprofile" component={UserProfile} />
-    </Switch>
+    <>
+      <ToastContainer bodyClassName="ToastBody" />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path={["/signin", "/signup"]} component={LandingPage} />
+        <PrivateRoute path="/home" component={UserHome} ChefComp={ChefHome} />
+        <PrivateRoute
+          path="/profile"
+          component={UserProfile}
+          ChefComp={ChefProfile}
+        />
+        <PrivateRoute path="/chef/:cid" component={FoodSubDetails} />
+        <Redirect to="/" />
+        {/* <PrivateRoute path="/userprofile" component={UserProfile} />
+        <PrivateRoute path="/foodsubdetails/:cid" component={FoodSubDetails} />
+        <PrivateRoute path="/chefProfile" component={ChefProfile} forChef />
+        <PrivateRoute path="/chefHome" component={ChefHome} forChef /> */}
+        {/* <PrivateRoute path="/userhome" component={UserHome} />
+        <PrivateRoute path="/userprofile" component={UserProfile} />
+        <PrivateRoute path="/foodsubdetails/:cid" component={FoodSubDetails} />
+        <PrivateRoute path="/chefProfile" component={ChefProfile} forChef />
+        <PrivateRoute path="/chefHome" component={ChefHome} forChef /> */}
+      </Switch>
+    </>
   );
 };
 
