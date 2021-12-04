467eeb5c54baf198a2f839d0531b840df58cbb76
Raj <varasaniraj5@gmail.com>
2021-12-04 21:36:22 +0530

    Integrated User Profile With backend
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>
    Co-authored-by: Harshil Mendpara <HarshilMendpara@users.noreply.github.com>

--- src/Containers/UserProfile/UserProfile.js
+++ src/Containers/UserProfile/UserProfile.js
@@ -2,13 +2,32 @@ import React, { useState, useEffect } from "react";
 
 import Styles from "./UserProfile.module.css";
 
+import UserSubList from "./../../Components/UserSubList/UserSubList";
 import Navbar from "./../../Components/Navbar/Navbar";
 import About from "./../../Components/About";
 import { useSelector } from "react-redux";
-import UserSubList from "./../../Components/UserSubList";
+import { getOrdersOfAUser } from "../../Services/order.service";
+import notify from "../../Utils/helper/notifyToast";
 
 function UserProfile() {
+
   const userData = useSelector((state) => state.userReducer.userData);
+  const accessToken = useSelector((state) => state.userReducer.accessToken);
+  const [orders, setOrders] = React.useState([]);
+
+  async function fetchUserOrders() {
+    try {
+      if (!accessToken) return;
+      const orderData = await getOrdersOfAUser(accessToken);
+      setOrders(orderData);
+    } catch (e) {
+      notify(e.response.data.errors[0].message, "error");
+    }
+  }
+
+  useEffect(() => {
+    fetchUserOrders()
+  }, [accessToken]);
 
   return (
     <div className={Styles.Wrapper}>
@@ -16,16 +35,12 @@ function UserProfile() {
       <div className={Styles.SubWrapper}>
         <div className={Styles.SubListWrapper}>
           <UserSubList
-          // data
+            subData={orders}
           />
         </div>
         <div className={Styles.Line} />
         <div className={Styles.AboutWraper}>
-          {userData.name ? (
-            <About userData={userData} isChef={false} />
-          ) : (
-            <About isChef={false} />
-          )}
+          {userData.name ? <About userData={userData} isChef={false} /> : <About isChef={false} />}
         </div>
       </div>
     </div>
